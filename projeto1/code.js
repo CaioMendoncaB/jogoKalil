ebabdbcd5c2370878e0a5f5edad69a60eab4b926
Gustavo10711 <gustavo.barreto@dcomp.ufs.br>
2024-10-16 23:50:34 -0300

    Add files via upload
    Criei 2 novas paginas de pergunta em html e implementei um timer para ser utilizado na transição de decolagem para minigame, que ainda precisa ser melhorado na parte do javascript

--- projeto1/code.js
+++ projeto1/code.js
@@ -1,19 +1,46 @@
-const muda = document.querySelector('.anima');
 
-
-function start() {
+const start = () => {
     window.location.href = "game.html";
 }
 
 
-function testa(){
+const testa = () => {
     const formulaEnviada = document.getElementById("form").value
     if(formulaEnviada=="resp3"){
-        window.location.href="fase.html";
+        window.location.href="game2.html";
     }
     if(formulaEnviada!=="resp3") {
         window.location.href="start.html";
     }
 
 }
+const testa2 = () => {
+    const formulaEnviada = document.getElementById("form").value
+    if(formulaEnviada=="resp4"){
+        window.location.href="game3.html";
+    }
+    if(formulaEnviada!=="resp4") {
+        window.location.href="start.html";
+    } 
+}
+const contagem = (x) => {
+    console.log(x)
+    if (x > 0) {
+        setTimeout(function() {
+        contagem(x - 1)}, 1000);
+    } 
+    else {
+        window.location.href="start.html";
+    }
+      }
+const testa3 = () => {
+    const formulaEnviada = document.getElementById("form").value
+    if(formulaEnviada=="resp1"){
+        window.location.href="fase.html";
+        contagem(2)
+    }
+    if(formulaEnviada!=="resp1") {
+        window.location.href="start.html";
+    }
 
+}
